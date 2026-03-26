b4b14ff94d8cc680c3a73ee5d93c26aa658b2efe
HELENE-AUERBACH <ln_au@yahoo.fr>
2026-03-26 01:06:57 -0400

    Task 3:getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the author parameter from the request URL and send the corresponding book's details
+    const author = req.params.author;
+    let books_details = Object.keys(books).map(function(key){
+        return books[key];
+    });
+    // Filter the books details array to find books whose author matches the extracted author parameter
+    let filtered_books_details = books_details.filter((book) => book.author === author);
+    // Send the filtered_books array as the response to the client
+    res.send(filtered_books_details);
 });
 
 // Get all books based on title
